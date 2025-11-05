9b328816ba995c0f0b128de316c9b2fee9d23cc6
naweabaasa <abaasanawe123@gmail.com>
2025-11-05 15:26:07 +0300

    practice questions uder functions

---
+++ part_a.py
@@ -0,0 +1,21 @@
+# 1.What is a function in Python?
+# A function is a block of reusable code that performs a specific task when you call it
+# They help to avoid repetition of code
+# A function is defined by a keyword "def"
+
+# 2.Explain the difference between parameter and argument. 
+# Parameter is a variable listed inside parentheses of a function while argument is a value that is sent to a function when it is called
+
+# 3.What does the return keyword do? 
+# The return key is used to send a value back to the caller
+
+#4.	What is the output of this code? 
+#def test(a, b=2):     
+    #return a * b 
+#print(test(3)) 
+# a and b are parameters while 3 and 2 are an arguments; b parameter is being assigned value 2, return a*b means a is being multiplied by b
+# So print(test(3)) means 3 is assigned to a thus a multiplied by 3
+# thus the output of the function will be 6
+
+#Reference
+#ClassNotes
---
+++ part_b.py
@@ -0,0 +1,29 @@
+# 1 Write a function to calculate the square of a number. 
+# def square(num):     
+# return num * num 
+
+def square(num):
+    return(num*2)
+print(square(4))
+
+# 2.Write a function that takes two numbers and returns their average. 
+# def average(a, b):     
+# return (a + b) / 2 
+def average(a,b):
+    return((a+b)/2)
+print(average(4, 2))
+
+# 3.  Write a function that checks if a number is even or odd. 
+# def even_or_odd(num):    
+    #if num % 2 == 0:         
+        #return "Even"     
+    # else: 
+        #return "Odd" 
+def even_or_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+print(even_or_odd(3))
+
+
